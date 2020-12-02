f8476cc193453b479e5e9941b88ac11b0e4eedb4
JorgeSerranoP <100405987@alumnos.uc3m.es>
2020-12-02 17:50:32 +0100

    Web Ivan y Jorge

---
+++ src/script/script.js
@@ -0,0 +1,338 @@
+function cambiarLogIn(){
+    document.getElementById("signup").style.display = "none";
+    document.getElementById("login").style.display = "block";
+    document.getElementById("logIn").className = "active";
+    document.getElementById("signUp").className = "";
+}
+
+function cambiarSignIn(){
+    document.getElementById("signup").style.display = "block";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("logIn").className = "";
+    document.getElementById("signUp").className = "active";
+}
+
+function showIni() {
+    document.getElementById("column_mid_ini").style.display = "block";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+}
+
+function showStudentsList() {
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "block";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+
+}
+
+function showForum() {
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "block";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+
+}
+
+function showForum1() {
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "block";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+
+}
+
+function showForum2() {
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "block";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+
+}
+
+function showForum3() {
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "block";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+
+}
+
+function showForum4() {
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "block";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+
+}
+
+function showForum5() {
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "block";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+
+}
+
+function showSubjectsList() {
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "block";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+}
+
+function showGrades() {
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "block";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "none";
+
+}
+
+function showGradesStudent(){
+    document.getElementById("column_mid_ini").style.display = "none";
+    document.getElementById("column_mid_studentsList").style.display = "none";
+    document.getElementById("column_mid_forum").style.display = "none";
+    document.getElementById("column_mid_grades").style.display = "none";
+    document.getElementById("column_mid_forum1").style.display = "none";
+    document.getElementById("column_mid_forum2").style.display = "none";
+    document.getElementById("column_mid_forum3").style.display = "none";
+    document.getElementById("column_mid_forum4").style.display = "none";
+    document.getElementById("column_mid_forum5").style.display = "none";
+    document.getElementById("column_mid_subjectsList").style.display = "none";
+    document.getElementById("column_mid_grades_student").style.display = "block";
+}
+
+function showStudent(){
+    document.getElementById("columnLeftComputerStudent").style.display = "block";
+    document.getElementById("columnLeftComputer").style.display = "none";
+}
+
+function showStudentPhone(){
+    if(getCookie("rol") == "Estudiante"){
+        if (window.matchMedia("(max-width: 600px)").matches) {
+            document.getElementById("columnLeftPhoneStudent").style.display = "block";
+            document.getElementById("columnLeftComputerStudent").style.display = "none";
+            document.getElementById("columnLeftPhone").style.display = "none";
+        } else{
+            document.getElementById("columnLeftPhoneStudent").style.display = "none";
+            document.getElementById("columnLeftComputerStudent").style.display = "block";
+        }
+    }
+}
+
+function showGrade(){
+    if(document.getElementById("rol").value == "Estudiante"){
+        document.getElementById("divGrade").style.display = "block";
+    } else{
+        document.getElementById("divGrade").style.display = "none";
+    }
+}
+
+function changeWeb(){
+    document.getElementById("pagInicio").style.display = "none";
+    document.getElementById("pagWeb").style.display = "block";
+    document.getElementById("userNameComputer").innerHTML = getCookie('usrname');
+    document.getElementById("userNameTablet").innerHTML = getCookie('usrname');
+}
+
+function cerrarSesion() {
+    if (confirm("Seguro que quieres cerrar sesión?")) {
+        document.getElementById("pagWeb").style.display = "none";
+        document.getElementById("pagInicio").style.display = "block";
+    }
+}
+
+function enviarEmail(){
+    window.open('mailto:test@example.com');
+}
+
+function descargarExcel(id_tabla){
+    var tmpElemento = document.createElement('a');
+    var data_type = 'data:application/vnd.ms-excel';
+    var tabla_div = document.getElementById(id_tabla);
+    var tabla_html = tabla_div.outerHTML.replace(/ /g, '%20');
+    tmpElemento.href = data_type + ', ' + tabla_html;
+    tmpElemento.download = 'calificaciones.xls';
+    tmpElemento.click();
+}
+
+function commentBox(id_comment, result){
+	var name = getCookie("name") + " " + getCookie("surname");
+    var comment = document.getElementById(id_comment).value;
+
+	if(name == "" || comment == ""){
+		alert("Los campos marcados con * son obligatorios!");
+	}else{
+        var parent=document.createElement("li");
+        var image_element = document.createElement("img");
+		var name_element=document.createElement("p");
+        var message_element=document.createElement("p");
+        var date_element=document.createElement("p");
+        
+		var txt_name=document.createTextNode(name);
+        var txt_message=document.createTextNode(comment);
+        var today = new Date();
+        var date = today.getDate()+"/"+(today.getMonth()+1)+"/"+today.getFullYear();
+        var time = today.getHours() + ":" + today.getMinutes();
+        var dateTime = date + " - " + time;
+        var txt_date = document.createTextNode(dateTime);
+
+        name_element.appendChild(txt_name);
+		message_element.appendChild(txt_message);
+        date_element.appendChild(txt_date);
+
+        parent.appendChild(image_element);
+        parent.appendChild(name_element);
+        parent.appendChild(message_element);
+        parent.appendChild(date_element);
+
+        image_element.setAttribute("src", "images/perfil.jpg"); 
+        image_element.setAttribute("class", "forumProfile"); 
+        name_element.setAttribute("class", "data");
+        message_element.setAttribute("class", "forumMessage");
+        
+		document.getElementById(result).appendChild(parent);
+	}
+}
+
+function checkCookie(){
+    var inputEmail = document.getElementById("emailLogin").value;
+    var cookieEmail = getCookie("email");
+    var inputPass = document.getElementById("passLogin").value;
+    var cookiePass = getCookie("pass");
+    if(inputEmail == cookieEmail){
+        if(inputPass == cookiePass){
+            if(getCookie("rol") == "Estudiante"){
+                changeWeb();
+                showStudent();
+            } else{
+                changeWeb();
+            }
+        } else{
+            alert("La contraseña es incorrecta");
+        }
+    } else{
+        alert("Este correo electrónico no está dado de alta");
+    }
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function saveCookies(){
+    if(document.getElementById("name").value == "" || document.getElementById("surname").value == "" || document.getElementById("usrname").value == "" 
+    || document.getElementById("nia").value == "" || document.getElementById("bornDate").value == "" || document.getElementById("dni").value == "" 
+    || document.getElementById("rol").value == "" || document.getElementById("lenguage").value == "" 
+    || document.getElementById("email").value == "" || document.getElementById("pass").value == ""){
+        alert("¡Debe rellenar todos los campos!");
+    } else{
+        setCookie("name", document.getElementById("name").value, 30);
+        setCookie("surname", document.getElementById("surname").value, 30);
+        setCookie("usrname", document.getElementById("usrname").value, 30);
+        setCookie("nia", document.getElementById("nia").value, 30);
+        setCookie("bornDate", document.getElementById("bornDate").value, 30);
+        setCookie("dni", document.getElementById("dni").value, 30);
+        setCookie("rol", document.getElementById("rol").value, 30);
+        setCookie("grade", document.getElementById("grade").value, 30);
+        setCookie("lenguage", document.getElementById("lenguage").value, 30);
+        if(document.getElementById("email").value == getCookie("email")){
+            alert("Ya existe una cuenta asociada al correo electrónico introducido, por favor introduzca uno válido");
+        } else{
+            setCookie("email", document.getElementById("email").value, 30);
+        }
+        setCookie("pass", document.getElementById("pass").value, 30);
+    }
+}
+
+window.addEventListener('resize', showStudentPhone);
